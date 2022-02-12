634a2901edf93c558c71d86bdcd5e385f9793d77
vivek22117 <vivekmishra22117@gmail.com>
2022-02-12 22:09:49 +0530

    adding TF resource for vault backend

---
+++ aws-tf-modules/vault-cluster-fixed-resources/db-resources.tf
@@ -0,0 +1,24 @@
+resource "aws_dynamodb_table" "vault_backend_table" {
+  name           = "${var.environment}-vault-backend-${var.default_region}"
+
+  read_capacity  = var.read_capacity
+  write_capacity = var.write_capacity
+
+  hash_key       = "Path"
+  range_key      = "Key"
+
+  attribute   {
+    name = "Path"
+    type = "S"
+  }
+  attribute {
+    name = "Key"
+    type = "S"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  tags = merge(local.common_tags, tomap({"Name"= "${var.environment}-tfstate-db"}))
+}
