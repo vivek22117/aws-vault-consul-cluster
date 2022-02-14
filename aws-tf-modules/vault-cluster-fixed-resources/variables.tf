f7cbf02a23f6804de55662411dfc8820dafc279c
vivek22117 <vivekmishra22117@gmail.com>
2022-02-14 20:43:07 +0530

    adding variables for vault key

--- aws-tf-modules/vault-cluster-fixed-resources/variables.tf
+++ aws-tf-modules/vault-cluster-fixed-resources/variables.tf
@@ -148,6 +148,23 @@ variable "recovery_window" {
   default     = 0
 }
 
+#####=======================================KMS Key Variables===========================#####
+variable "kms_key_deletion_window" {
+  type        = number
+  description = "Duration in days after which the key is deleted after destruction of the resource (must be between 7 and 30 days)."
+}
+
+variable "resource_name_prefix" {
+  type        = string
+  description = "Resource name prefix used for tagging and naming AWS resources"
+}
+
+variable "user_supplied_kms_key_arn" {
+  type        = string
+  description = "(OPTIONAL) User-provided KMS key ARN. Providing this will disable the KMS submodule from generating a KMS key used for Vault auto-unseal"
+  default     = null
+}
+
 #####==============Local variables======================#####
 locals {
   common_tags = {
