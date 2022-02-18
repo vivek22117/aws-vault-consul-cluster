823d7cd34b674b23918ef5e65650eb362dbb89ec
vivek22117 <vivekmishra22117@gmail.com>
2022-02-18 11:28:13 +0530

    adding variables for vault LB

--- aws-tf-modules/vault-cluster-fixed-resources/variables.tf
+++ aws-tf-modules/vault-cluster-fixed-resources/variables.tf
@@ -21,41 +21,41 @@ variable "component_name" {
 
 #####======================DynamoDB Table Variables=========================#####
 variable "read_capacity" {
-  type = number
+  type        = number
   description = "RCU for dynamoDB table"
 }
 
 variable "write_capacity" {
-  type = number
+  type        = number
   description = "RCU for dynamoDB table"
 }
 
 #####========================Secret Key Variables==============================#####
 variable "private_key_algorithm" {
-  type = string
+  type        = string
   description = "The name of the algorithm to use for private keys. Must be one of: RSA or ECDSA."
   default     = "RSA"
 }
 
 variable "private_key_rsa_bits" {
-  type = string
+  type        = string
   description = "The size of the generated RSA key in bits. Should only be used if var.private_key_algorithm is RSA."
   default     = "2048"
 }
 
 variable "validity_period_hours" {
-  type = number
+  type        = number
   description = "Number of hours the CA certificate is valid"
 }
 
 
 variable "ca_common_name" {
-  type = string
+  type        = string
   description = "CA cert common name"
 }
 
 variable "organization_name" {
-  type = string
+  type        = string
   description = "Organization name for the CA cert"
 }
 
@@ -72,32 +72,32 @@ variable "ca_allowed_uses" {
 }
 
 variable "ca_public_key_file_path" {
-  type = string
+  type        = string
   description = "Write the PEM-encoded CA certificate public key to this path (e.g. /etc/tls/ca.crt.pem)."
-  default = "tls/ca.crt.pem"
+  default     = "tls/ca.crt.pem"
 }
 
 variable "public_key_file_path" {
-  type = string
+  type        = string
   description = "Write the PEM-encoded certificate public key to this path (e.g. /etc/tls/vault.crt.pem)."
-  default = "tls/vault.crt.pem"
+  default     = "tls/vault.crt.pem"
 }
 
 variable "private_key_file_path" {
-  type = string
+  type        = string
   description = "Write the PEM-encoded certificate private key to this path (e.g. /etc/tls/vault.key.pem)."
-  default = "tls/vault.key.pem"
+  default     = "tls/vault.key.pem"
 }
 
 variable "permissions" {
-  type = string
+  type        = string
   description = "The Unix file permission to assign to the cert files (e.g. 0600)."
   default     = "0600"
 }
 
 variable "owner" {
   description = "The OS user who should be given ownership over the certificate files."
-  default = "vivekm"
+  default     = "vivekm"
 }
 
 variable "ip_addresses" {
@@ -110,7 +110,7 @@ variable "ip_addresses" {
 
 variable "common_name" {
   description = "The common name to use in the subject of the certificate (e.g. acme.co cert)."
-  default = "doubledigit-solutions cert"
+  default     = "doubledigit-solutions cert"
 }
 
 
@@ -205,6 +205,11 @@ variable "allowed_inbound_cidrs" {
 variable "vault_cluster_sg_id" {
   type        = string
   description = "Security group ID of Vault cluster"
+}
+
+variable "lb_subnets" {
+  type        = list(string)
+  description = "Subnets where load balancer will be deployed"
 }
 
 #####==============Local variables======================#####
