a36b361c230b7400d437a7810a8747502acee792
vivek22117 <vivekmishra22117@gmail.com>
2022-06-20 21:01:18 +0530

    adding resources to create ACM certificate for ALB and its validation

---
+++ aws-tf-modules/vault-cluster-fixed-resources/route53.tf
@@ -0,0 +1,73 @@
+data "aws_route53_zone" "public" {
+  name         = var.domain
+  private_zone = false
+}
+
+# This creates an SSL certificate
+resource "aws_acm_certificate" "vault_cluster_certificate" {
+
+  domain_name               = "${var.cluster_dns}.${var.domain}"
+  subject_alternative_names = ["www.${var.cluster_dns}.${var.domain}"]
+  validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# This is a DNS record for the ACM certificate validation to prove we own the domain
+resource "aws_route53_record" "cert_validation" {
+  depends_on = [aws_acm_certificate.vault_cluster_certificate]
+
+  for_each = {
+    for dvo in aws_acm_certificate.vault_cluster_certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.public.id
+  ttl             = 300
+}
+
+# This tells terraform to cause the route53 validation to happen
+resource "aws_acm_certificate_validation" "cert" {
+  depends_on = [aws_route53_record.cert_validation, aws_acm_certificate.vault_cluster_certificate]
+
+  timeouts {
+    create = "20m"
+  }
+
+  certificate_arn         = aws_acm_certificate.vault_cluster_certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+
+
+resource "aws_route53_record" "portfolio_web_1" {
+  zone_id = data.aws_route53_zone.public.zone_id
+  name    = "${var.cluster_dns}.${var.domain}"
+  type    = "A"
+
+  alias {
+    name                   = aws_alb.vault_alb.dns_name
+    zone_id                = aws_alb.vault_alb.zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "portfolio_web_2" {
+  zone_id = data.aws_route53_zone.public.zone_id
+  name    = "www.${var.cluster_dns}.${var.domain}"
+  type    = "A"
+
+  alias {
+    name                   = aws_alb.vault_alb.dns_name
+    zone_id                = aws_alb.vault_alb.zone_id
+    evaluate_target_health = false
+  }
+}
