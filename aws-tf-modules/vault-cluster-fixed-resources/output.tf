4dbb4c2cd047ddbf0b81f4362fe7c3de7f21573a
vivek22117 <vivekmishra22117@gmail.com>
2022-06-21 10:36:27 +0530

    updating resoruce name in the tf output file

--- aws-tf-modules/vault-cluster-fixed-resources/output.tf
+++ aws-tf-modules/vault-cluster-fixed-resources/output.tf
@@ -12,20 +12,17 @@ output "private_key_file_path" {
 
 output "lb_certificate_arn" {
   description = "ARN of ACM cert to use with Vault LB listener"
-  value       = aws_acm_certificate.vault.arn
+  value       = aws_acm_certificate.vault_cluster_certificate.arn
 }
-
 
 output "secrets_manager_arn" {
   description = "ARN of secrets_manager secret"
   value       = aws_secretsmanager_secret.vault_tls.arn
 }
 
-
 output "kms_key_arn" {
-  value = var.user_supplied_kms_key_arn != null ? var.user_supplied_kms_key_arn : aws_kms_key.vault_key[0].arn
+  value = var.user_supplied_kms_key_arn != null ? var.user_supplied_kms_key_arn : aws_kms_key.vault_cluster_key[0].arn
 }
-
 
 output "s3_bucket_vault_license_arn" {
   value = aws_s3_bucket.vault_license_bucket.arn
