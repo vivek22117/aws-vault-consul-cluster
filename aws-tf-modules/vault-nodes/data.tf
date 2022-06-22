93b9db578d4ec9b24a95709d9676142fbf2a351f
vivek22117 <vivekmishra22117@gmail.com>
2022-06-22 09:33:02 +0530

    adding data resource to read ubuntu AMI for vault cluster

--- aws-tf-modules/vault-nodes/data.tf
+++ aws-tf-modules/vault-nodes/data.tf
@@ -1,3 +1,20 @@
+data "aws_ami" "ubuntu_ami" {
+  count       = var.user_supplied_ami_id != null ? 0 : 1
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 ###################################################
 # Fetch remote state for S3 deployment bucket     #
 ###################################################
@@ -7,6 +24,16 @@ data "terraform_remote_state" "vault_resources" {
   config = {
     bucket = "${var.environment}-tfstate-${data.aws_caller_identity.current.account_id}-${var.default_region}"
     key    = "state/${var.environment}/vault-resources/terraform.tfstate"
+    region = var.default_region
+  }
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket = "${var.environment}-tfstate-${data.aws_caller_identity.current.account_id}-${var.default_region}"
+    key    = "state/${var.environment}/vpc/terraform.tfstate"
     region = var.default_region
   }
 }
