b8ba92e776125c184bfe982632511efe9f181cc4
vivek22117 <vivekmishra22117@gmail.com>
2022-06-29 19:53:34 +0530

    removing unused output variables

--- aws-tf-modules/module.vault-cluster-fixed-resources/output.tf
+++ aws-tf-modules/module.vault-cluster-fixed-resources/output.tf
@@ -1,15 +1,3 @@
-output "ca_public_key_file_path" {
-  value = var.ca_public_key_file_path
-}
-
-output "public_key_file_path" {
-  value = var.public_key_file_path
-}
-
-output "private_key_file_path" {
-  value = var.private_key_file_path
-}
-
 output "lb_certificate_arn" {
   description = "ARN of ACM cert to use with Vault LB listener"
   value       = aws_acm_certificate.vault_cluster_certificate.arn
