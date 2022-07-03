7e13da98cac57911e95f989b6a9225cd7127bc02
vivek22117 <vivekmishra22117@gmail.com>
2022-07-03 09:01:47 +0530

    updating output file for vault fixed resources

--- aws-tf-deployment/vault-cluster-fixed-resources/output.tf
+++ aws-tf-deployment/vault-cluster-fixed-resources/output.tf
@@ -11,3 +11,33 @@ output "secrets_manager_arn" {
 output "vault_instance_profile" {
   value = module.vault_cluster_resources.vault_instance_profile
 }
+
+output "kms_key_arn" {
+  value = module.vault_cluster_resources.kms_key_arn
+}
+
+output "s3_bucket_vault_license_arn" {
+  value = module.vault_cluster_resources.s3_bucket_vault_license_arn
+}
+
+output "s3_bucket_vault_license" {
+  value = module.vault_cluster_resources.s3_bucket_vault_license
+}
+
+output "vault_license_name" {
+  value = module.vault_cluster_resources.vault_license_name
+}
+
+output "vault_lb_sg" {
+  value = module.vault_cluster_resources.vault_lb_sg
+}
+
+output "vault_cluster_sg" {
+  value = module.vault_cluster_resources.vault_cluster_sg
+}
+
+
+output "vault_lb_target_group_arn" {
+  value = module.vault_cluster_resources.vault_lb_target_group_arn
+}
+
