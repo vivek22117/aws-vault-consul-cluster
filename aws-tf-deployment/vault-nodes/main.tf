3a0ad160e037b1e049a9b8544cb865f894a4a891
vivek22117 <vivekmishra22117@gmail.com>
2022-07-06 21:32:46 +0530

    updating TF main script

--- aws-tf-deployment/vault-nodes/main.tf
+++ aws-tf-deployment/vault-nodes/main.tf
@@ -4,17 +4,17 @@
 module "vault_node_resources" {
   source = "../../aws-tf-modules/module.vault-nodes"
 
-  asg_health_check_grace_period = ""
-  asg_wait_for_elb_capacity     = ""
-  default_cooldown              = 0
-  default_region                = ""
-  environment                   = ""
-  health_check_type             = ""
-  kms_key_arn                   = ""
-  max_price                     = ""
-  owner_team                    = ""
-  resource_name_prefix          = ""
-  secrets_manager_arn           = ""
-  termination_policies          = []
-  wait_for_capacity_timeout     = ""
+  asg_health_check_grace_period = var.asg_health_check_grace_period
+  asg_wait_for_elb_capacity     = var.asg_wait_for_elb_capacity
+  default_cooldown              = var.default_cooldown
+  default_region                = var.default_region
+  environment                   = var.environment
+  health_check_type             = var.health_check_type
+  kms_key_arn                   = var.kms_key_arn
+  max_price                     = var.max_price
+  owner_team                    = var.owner_team
+  resource_name_prefix          = var.resource_name_prefix
+  secrets_manager_arn           = var.secrets_manager_arn
+  termination_policies          = var.termination_policies
+  wait_for_capacity_timeout     = var.wait_for_capacity_timeout
 }
